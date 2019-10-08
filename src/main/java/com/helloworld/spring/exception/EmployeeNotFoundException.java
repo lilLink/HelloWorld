df7e5a0d77c2df15dae0dbeb531d4988f1b065e2
lilLink <lillinkwrk@gmail.com>
2019-10-08 20:44:11 +0300

    Fixed Application

--- src/main/java/com/helloworld/spring/exception/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.helloworld.spring.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(Long id){
-        super("Could not find employee" + id);
-    }
-}
